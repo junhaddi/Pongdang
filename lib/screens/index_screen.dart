967eb40d38b6b5b6a98dfe23c45aa1a65d167560
junhaddi <rkdwnsgk05@gmail.com>
2020-05-24 15:52:48 +0900

    페이지뷰 구현

--- lib/screens/index_screen.dart
+++ lib/screens/index_screen.dart
@@ -1,11 +1,78 @@
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:pongdang/pages/calendar_page.dart';
+import 'package:pongdang/pages/home_page.dart';
+import 'package:pongdang/pages/setting_page.dart';
+import 'package:pongdang/pages/timer_page.dart';
 
-class IndexScreen extends StatelessWidget {
+class IndexScreen extends StatefulWidget {
+  @override
+  _IndexScreenState createState() => _IndexScreenState();
+}
+
+class _IndexScreenState extends State<IndexScreen> {
+  int _currentIndex = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('홈'),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() => _currentIndex = index);
+        },
+        children: <Widget>[
+          HomePage(),
+          CalendarPage(),
+          TimerPage(),
+          SettingPage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.jumpToPage(index);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+            icon: Icon(Icons.home),
+            title: Text('홈'),
+            activeColor: Colors.redAccent,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.event),
+            title: Text('캘린더'),
+            activeColor: Colors.blueAccent,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.play_circle_filled),
+            title: Text('타이머'),
+            activeColor: Colors.deepPurpleAccent,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('설정'),
+            activeColor: Colors.orangeAccent,
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
