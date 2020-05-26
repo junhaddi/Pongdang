92cc0e81a091fbbab9a37007e581b580e42b68aa
junhaddi <rkdwnsgk05@gmail.com>
2020-05-26 22:17:52 +0900

    프로젝트 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:pongdang/screens/index_screen.dart';
-import 'package:pongdang/screens/walkthrough_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
@@ -20,50 +18,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DynamicTheme(
-      defaultBrightness: (prefs.getBool('isDark') ?? false
-          ? Brightness.dark
-          : Brightness.light),
-      data: (brightness) {
-        return brightness == Brightness.light
-            ? ThemeData(
-                // 기본모드 테마
-                primarySwatch: Colors.grey,
-                primaryColor: Colors.white,
-                brightness: Brightness.light,
-                accentColor: Colors.black,
-                fontFamily: 'NanumBarunGothic',
-              )
-            : ThemeData(
-                // 다크모드 테마
-                primarySwatch: Colors.grey,
-                primaryColor: Colors.black,
-                brightness: Brightness.dark,
-                accentColor: Colors.white,
-                fontFamily: 'NanumBarunGothic',
-              );
-      },
-      themedWidgetBuilder: (context, theme) {
-        return MaterialApp(
-          title: '퐁당퐁당',
-          theme: theme,
-          debugShowCheckedModeBanner: false,
-          routes: <String, WidgetBuilder>{
-            '/index': (BuildContext context) => IndexScreen(),
-          },
-          home: _handleCurrentScreen(),
-        );
-      },
+    return MaterialApp(
+      title: '퐁당퐁당',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        primaryColor: Colors.white,
+        brightness: Brightness.light,
+        accentColor: Colors.black,
+        fontFamily: 'NanumBarunGothic',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: IndexScreen(prefs: prefs),
     );
-  }
-
-  // 앱 실행 시 스크린 라우팅
-  Widget _handleCurrentScreen() {
-    bool isWalkthroughSeen = (prefs.getBool('walkthroughSeen') ?? false);
-    if (isWalkthroughSeen) {
-      return IndexScreen();
-    } else {
-      return WalkthroughScreen(prefs: prefs);
-    }
   }
 }
--- lib/screens/index_screen.dart
+++ lib/screens/index_screen.dart
@@ -1,31 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:pongdang/pages/calendar_page.dart';
-import 'package:pongdang/pages/home_page.dart';
-import 'package:pongdang/pages/setting_page.dart';
-import 'package:pongdang/pages/timer_page.dart';
-import 'package:pongdang/widgets/bottom_navy_bar.dart';
 import 'package:pongdang/widgets/custom_dialog.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class IndexScreen extends StatefulWidget {
+  final SharedPreferences prefs;
+
+  IndexScreen({this.prefs});
+
   @override
   _IndexScreenState createState() => _IndexScreenState();
 }
 
 class _IndexScreenState extends State<IndexScreen> {
-  int _currentIndex = 0;
-  PageController _pageController;
   DateTime _currentBackPressTime;
 
   @override
   void initState() {
     super.initState();
-    _pageController = PageController();
   }
 
   @override
   void dispose() {
-    _pageController.dispose();
     super.dispose();
   }
 
@@ -34,51 +30,9 @@ class _IndexScreenState extends State<IndexScreen> {
     return Scaffold(
       body: WillPopScope(
         onWillPop: _onWillPop,
-        child: PageView(
-          controller: _pageController,
-          onPageChanged: (index) {
-            setState(() => _currentIndex = index);
-          },
-          children: <Widget>[
-            HomePage(),
-            CalendarPage(),
-            TimerPage(),
-            SettingPage(),
-          ],
+        child: Center(
+          child: Text('홈'),
         ),
-      ),
-      bottomNavigationBar: BottomNavyBar(
-        selectedIndex: _currentIndex,
-        onItemSelected: (index) {
-          setState(() => _currentIndex = index);
-          _pageController.jumpToPage(index);
-        },
-        items: <BottomNavyBarItem>[
-          BottomNavyBarItem(
-            icon: Icon(Icons.home),
-            title: Text('홈'),
-            activeColor: Colors.redAccent,
-            textAlign: TextAlign.center,
-          ),
-          BottomNavyBarItem(
-            icon: Icon(Icons.event),
-            title: Text('캘린더'),
-            activeColor: Colors.blueAccent,
-            textAlign: TextAlign.center,
-          ),
-          BottomNavyBarItem(
-            icon: Icon(Icons.play_circle_filled),
-            title: Text('타이머'),
-            activeColor: Colors.deepPurpleAccent,
-            textAlign: TextAlign.center,
-          ),
-          BottomNavyBarItem(
-            icon: Icon(Icons.settings),
-            title: Text('설정'),
-            activeColor: Colors.orangeAccent,
-            textAlign: TextAlign.center,
-          ),
-        ],
       ),
     );
   }
@@ -102,7 +56,6 @@ class _IndexScreenState extends State<IndexScreen> {
           subtitle: '퐁당퐁당을 종료할까요?',
           child: Container(
             height: 200.0,
-            // TODO AdMob Native 구현
           ),
           event: () {
             SystemChannels.platform.invokeMethod('SystemNavigator.pop');
